00ea197a5e8f0f212032226bbfb249b5752fef7a
Pratik Gorade <pratikgorade0@gmail.com>
2021-03-27 20:00:58 +0530

    seperated parse user and parse post functions from get_posts so i can reuse them
    Signed-off-by: Pratik Gorade <pratikgorade0@gmail.com>

--- lib-sters/src/lib.rs
+++ lib-sters/src/lib.rs
@@ -32,7 +32,7 @@ fn generate_url(path: LobstersPath, page: Option<u32>) -> String {
     }
 }
 
-pub fn generate_posts(path: LobstersPath, page: Option<u32>) -> Option<Vec<Post>> {
+pub fn get_posts(path: LobstersPath, page: Option<u32>) -> Option<Vec<Post>> {
     let url = generate_url(path, page);
     let responce: Response = minreq::get(url).send().unwrap();
     let res_str: &str = responce.as_str().unwrap();
@@ -40,109 +40,123 @@ pub fn generate_posts(path: LobstersPath, page: Option<u32>) -> Option<Vec<Post>
     let obj_vec: &Vec<Value> = json_value.as_array().unwrap();
     let mut posts: Vec<Post> = Vec::new();
     for post_obj in obj_vec {
-        let mut post: Post = Post::default();
-        let mut user: User = User::default();
-        let mut tags: Vec<String> = Vec::new();
-        for user_obj in post_obj.get("submitter_user") {
-            user.username = user_obj
-                .get("username")
-                .unwrap()
-                .as_str()
-                .unwrap()
-                .to_owned();
-            user.created_at = user_obj
-                .get("created_at")
-                .unwrap()
-                .as_str()
-                .unwrap()
-                .to_owned();
-            user.is_admin = user_obj.get("is_admin").unwrap().as_bool().unwrap();
-            match user_obj.get("about") {
-                Some(about) => {
-                    user.about = Some(about.as_str().unwrap().to_owned());
-                }
-                None => {}
-            };
-            user.is_moderator = user_obj.get("is_moderator").unwrap().as_bool().unwrap();
-            match user_obj.get("karma") {
-                Some(karma) => {
-                    user.karma = Some(karma.as_i64().unwrap() as i32);
-                }
-                None => {}
-            };
-            match user_obj.get("avtar_url") {
-                Some(avtar_url) => {
-                    user.avtar_url = Some(avtar_url.as_str().unwrap().to_owned());
-                }
-                None => {}
-            };
-            user.invited_by_user = user_obj
-                .get("invited_by_user")
-                .unwrap()
-                .as_str()
-                .unwrap()
-                .to_owned();
-            match user_obj.get("github_username") {
-                Some(github_username) => {
-                    user.github_username = Some(github_username.as_str().unwrap().to_owned());
-                }
-                None => {}
-            };
-            match user_obj.get("twitter_username") {
-                Some(twitter_username) => {
-                    user.twitter_username = Some(twitter_username.as_str().unwrap().to_owned());
-                }
-                None => {}
-            };
-        }
-        post.short_id = post_obj
-            .get("short_id")
-            .unwrap()
-            .as_str()
-            .unwrap()
-            .to_owned();
-        post.short_id_url = post_obj
-            .get("short_id_url")
-            .unwrap()
-            .as_str()
-            .unwrap()
-            .to_owned();
-        post.created_at = post_obj
-            .get("created_at")
-            .unwrap()
-            .as_str()
-            .unwrap()
-            .to_owned();
-        post.title = post_obj.get("title").unwrap().as_str().unwrap().to_owned();
-        post.url = post_obj.get("url").unwrap().as_str().unwrap().to_owned();
-        post.score = post_obj.get("score").unwrap().as_i64().unwrap() as i32;
-        post.flags = post_obj.get("flags").unwrap().as_i64().unwrap() as u32;
-        match post_obj.get("comment_count") {
-            Some(comment_count) => {
-                post.comment_count = Some(comment_count.as_i64().unwrap() as u32);
-            }
-            None => {}
-        };
-        match post_obj.get("description") {
-            Some(description) => {
-                post.description = Some(description.as_str().unwrap().to_owned());
-            }
-            None => {}
-        };
-        post.comments_url = post_obj
-            .get("comments_url")
-            .unwrap()
-            .as_str()
-            .unwrap()
-            .to_owned();
-        post.submitter_user = user;
-        for tag in post_obj.get("tags").unwrap().as_array().unwrap() {
-            tags.push(tag.as_str().unwrap().to_owned())
-        }
-        post.tags = Some(tags);
+        let post = parse_post(post_obj);
         posts.push(post);
     }
     Some(posts)
+}
+
+#[test]
+fn test_get_post() {
+    let posts = get_posts(LobstersPath::Hottest, None).unwrap();
+    assert_ne!(posts.len(), 0)
+}
+
+fn parse_post(post_obj: &Value) -> Post {
+    let mut post: Post = Post::default();
+    let mut tags: Vec<String> = Vec::new();
+    let user_obj = post_obj.get("submitter_user").unwrap();
+    post.short_id = post_obj
+        .get("short_id")
+        .unwrap()
+        .as_str()
+        .unwrap()
+        .to_owned();
+    post.short_id_url = post_obj
+        .get("short_id_url")
+        .unwrap()
+        .as_str()
+        .unwrap()
+        .to_owned();
+    post.created_at = post_obj
+        .get("created_at")
+        .unwrap()
+        .as_str()
+        .unwrap()
+        .to_owned();
+    post.title = post_obj.get("title").unwrap().as_str().unwrap().to_owned();
+    post.url = post_obj.get("url").unwrap().as_str().unwrap().to_owned();
+    post.score = post_obj.get("score").unwrap().as_i64().unwrap() as i32;
+    post.flags = post_obj.get("flags").unwrap().as_i64().unwrap() as u32;
+    match post_obj.get("comment_count") {
+        Some(comment_count) => {
+            post.comment_count = Some(comment_count.as_i64().unwrap() as u32);
+        }
+        None => {}
+    };
+    match post_obj.get("description") {
+        Some(description) => {
+            post.description = Some(description.as_str().unwrap().to_owned());
+        }
+        None => {}
+    };
+    post.comments_url = post_obj
+        .get("comments_url")
+        .unwrap()
+        .as_str()
+        .unwrap()
+        .to_owned();
+    post.submitter_user = parse_user(user_obj);
+    for tag in post_obj.get("tags").unwrap().as_array().unwrap() {
+        tags.push(tag.as_str().unwrap().to_owned())
+    }
+    post.tags = Some(tags);
+    post
+}
+
+fn parse_user(user_obj: &Value) -> User {
+    let mut user: User = User::default();
+    user.username = user_obj
+        .get("username")
+        .unwrap()
+        .as_str()
+        .unwrap()
+        .to_owned();
+    user.created_at = user_obj
+        .get("created_at")
+        .unwrap()
+        .as_str()
+        .unwrap()
+        .to_owned();
+    user.is_admin = user_obj.get("is_admin").unwrap().as_bool().unwrap();
+    match user_obj.get("about") {
+        Some(about) => {
+            user.about = Some(about.as_str().unwrap().to_owned());
+        }
+        None => {}
+    };
+    user.is_moderator = user_obj.get("is_moderator").unwrap().as_bool().unwrap();
+    match user_obj.get("karma") {
+        Some(karma) => {
+            user.karma = Some(karma.as_i64().unwrap() as i32);
+        }
+        None => {}
+    };
+    match user_obj.get("avtar_url") {
+        Some(avtar_url) => {
+            user.avtar_url = Some(avtar_url.as_str().unwrap().to_owned());
+        }
+        None => {}
+    };
+    user.invited_by_user = user_obj
+        .get("invited_by_user")
+        .unwrap()
+        .as_str()
+        .unwrap()
+        .to_owned();
+    match user_obj.get("github_username") {
+        Some(github_username) => {
+            user.github_username = Some(github_username.as_str().unwrap().to_owned());
+        }
+        None => {}
+    };
+    match user_obj.get("twitter_username") {
+        Some(twitter_username) => {
+            user.twitter_username = Some(twitter_username.as_str().unwrap().to_owned());
+        }
+        None => {}
+    };
+    user
 }
 
 #[cfg(test)]
